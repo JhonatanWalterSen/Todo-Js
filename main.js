60a6ffdf79f572726ff437656a009070cfc1430f
JhonatanWalterSen <jhonatanarizaq@gmail.com>
2020-07-13 02:49:16 -0500

    Render()

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 const todos = [];
-
+const render = () => {
+    const todoList = document.getElementById('todo-list')
+        // todoList.innerHTML= '';
+        // for (let i = 0; i < todos.length; i++) {
+        //     todoList.innerHTML += '<li>' + todos[i] +'</li>';
+        // }
+    const todosTemplate = todos.map(t => '<li>' + t +'</li>');
+    todoList.innerHTML = todosTemplate.join(''); //join toma todos los elementos y los junta
+    const elementos = document.querySelectorAll('#todo-list li');
+    elementos.forEach((elemento, i) => {
+        console.log(elemento,i);
+        elemento.addEventListener('click',() => {
+            elemento.parentNode.removeChild(elemento)
+            todos.splice(i,1);
+            render()
+        })
+    })
+}
 window.onload = () => { 
    const form = document.getElementById('todo-form')
    form.onsubmit = (e) => {
@@ -21,7 +38,9 @@ window.onload = () => {
         elementos.forEach((elemento, i) => {
             console.log(elemento,i);
             elemento.addEventListener('click',() => {
-                console.log(elemento,i)
+                elemento.parentNode.removeChild(elemento)
+                todos.splice(i,1)
+                render()
             })
         })
    }
